edcfe21f1c3c85116e9513cf7667f40c71ef0184
devopsmatic <138186195+devopsmatic@users.noreply.github.com>
2025-06-11 18:36:12 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install apache2 php git -y
 sudo systemctl enable apache2.service
 sudo systemctl start apache2
-sudo git clone https://github.com/fastrackcloud/website-automation.git
+sudo git clone https://github.com/devopsmatic/website-automation.git
 sudo cp ./website-automation/* /var/www/html
 sudo mv /var/www/html/htaccess /var/www/html/.htaccess
 sudo sed -i '19iDirectoryIndex index.php /html/index.php' /etc/apache2/sites-available/000-default.conf 
